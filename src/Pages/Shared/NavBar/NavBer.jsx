6f91c89302bcebfd297db31e9cc0eba080109c50
md fuwad <fuhadrare444@gmail.com>
2024-03-30 01:48:03 +0600

    add navBar

--- src/Pages/Shared/NavBar/NavBer.jsx
+++ src/Pages/Shared/NavBar/NavBer.jsx
@@ -1,11 +1,58 @@
-
-
 const NavBer = () => {
-    return (
-        <div>
-            <h1>This is nav bar</h1>
+  const naveOption = (
+    <>
+     <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+    </>
+  );
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+            {naveOption}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Bistro Bass</a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+           {naveOption}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBer;
